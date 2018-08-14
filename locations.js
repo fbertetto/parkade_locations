09ffc16a7efe8a5dc4a6116c43882f1b37912e61
Fabian Bertetto <fabianbertetto@gmail.com>
2018-08-14 17:30:07 -0300

    add real data

--- locations.js
+++ locations.js
@@ -4,56 +4,56 @@ eqfeed_callback({
 {
       "type":"Feature",
       "properties":{
-        "name":"Parking City",
-        "address": "Tansito Caseres 415"
+        "name":"Estacionamiento Belgrano",
+        "address": "Belgrano 170"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.191339, 
-          -31.411388
+          -64.188942, 
+          -31.416747
         ]
       }
     },
     {
       "type":"Feature",
       "properties":{
-        "name":"Parking Mania",
-        "address": "Caseros 686"
+        "name":"Estacionamiento Tucuman",
+        "address": "Tucuman 236"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.197579, 
-          -31.417798
+          -64.186957, 
+          -31.412041
         ]
       }
     },
     {
       "type":"Feature",
       "properties":{
-        "name":"Test Parking",
-        "address": "Pasaje Rincon 415"
+        "name":"Estacionamiento Municipalidad",
+        "address": "Plaza de la intendencia"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.189789, 
-          -31.418798
+          -64.191908, 
+          -31.417132
         ]
       }
     },
     {
       "type":"Feature",
       "properties":{
-        "name":"Playa por hora",
-        "address": "San Juan 2020"
+        "name":"Estación 27",
+        "address": "27 De Abril 364"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.1908797, 
-          -31.420809
+          -64.189376, 
+          -31.415644
         ]
       }
     },
@@ -74,28 +74,154 @@ eqfeed_callback({
     {
       "type":"Feature",
       "properties":{
-        "name":"Playa 24hs",
-        "address": "Marcelo T. Alvear 654"
+        "name":"Playa Bolivar",
+        "address": "General Simón Bolívar 353"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.190182370, 
-          -31.4278685
+          -64.192305, 
+          -31.418006
         ]
       }
     },
     {
       "type":"Feature",
       "properties":{
-        "name":"Pairking",
-        "address": "Rondeau 18"
+        "name":"Parking oulton",
+        "address": "Velez Sarsfield 570"
       },
       "geometry":{
         "type":"Point",
         "coordinates":[
-          -64.1897238912, 
-          -31.42597897
+          -64.189635, 
+          -31.422012
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Playa Estacionamiento Duarte Quirós",
+        "address": "Duarte Quirós 269"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.189249, 
+          -31.418395
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Mercado Sur",
+        "address": "Blvd. Pte. Arturo Umberto Illia 156"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.184305, 
+          -31.421480
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Capuchinos",
+        "address": "San Lorenzo 140"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.185378, 
+          -31.423760
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Ituzaingó",
+        "address": "Ituzaingó 619"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.184595, 
+          -31.424383
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Artigas",
+        "address": "General Artigas 333"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.196260, 
+          -31.416845
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Don Tunin",
+        "address": "Av Duarte Quirós 1068"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.199811, 
+          -31.415087
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento L&L",
+        "address": "Av Vélez Sarsfield 869"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.191087, 
+          -31.425400
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Paraná",
+        "address": "Paraná 560"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.180200, 
+          -31.424649
+        ]
+      }
+    },
+    {
+      "type":"Feature",
+      "properties":{
+        "name":"Estacionamiento Balcarce 443",
+        "address": "Balcarce 443"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[
+          -64.178237, 
+          -31.422726
         ]
       }
     }
